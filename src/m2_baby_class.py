ea30d5f21f0a3e4a7fac683eba0eca11d00c0169
wolfead <wolfead@rose-hulman.edu>
2018-12-17 11:37:08 -0500

    finished

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -3,8 +3,8 @@ A   Baby   class and functions that use/test it.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Alex Wolfe.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -42,7 +42,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 2.
+# done: 2.
 #
 #  Step 2a:  Implement a class called   Baby   that has a constructor method
 #            (__init__) and two other methods, as described below.
@@ -107,8 +107,22 @@ def main():
 #          """ Brief description of what an object of the class 'is'. """
 #
 ###############################################################################
-
-
+class Baby(object):
+    def __init__(self,baby_name):
+        self.babyname = baby_name
+        self.count = 0
+        print('Hello baby',self.babyname,'!')
+    def feed_baby(self):
+        print('Thank you for feeding baby',self.babyname)
+        self.count = 0
+    def hour_passes(self):
+        self.count = self.count + 1
+        if self.count == 1:
+            print('Baby', self.babyname,'is sleeping.')
+        elif self.count == 2:
+            print('Baby',self.babyname,'is awake. Time for food')
+        else:
+            print('Baby',self.babyname,'is CRYING uncontrollably! Feed the baby!')
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
